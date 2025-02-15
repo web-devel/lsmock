2df43843319419a3316a82c629ecb500d4857b3f
Vlad Minaev <vlad.minaev@gmail.com>
2025-02-16 01:43:44 +0200

    Completion dummy

--- src/main/kotlin/me/webdevel/lsmock/MockTextDocumentService.kt
+++ src/main/kotlin/me/webdevel/lsmock/MockTextDocumentService.kt
@@ -1,10 +1,15 @@
 package me.webdevel.lsmock
 
+import org.eclipse.lsp4j.CompletionItem
+import org.eclipse.lsp4j.CompletionList
+import org.eclipse.lsp4j.CompletionParams
 import org.eclipse.lsp4j.DidChangeTextDocumentParams
 import org.eclipse.lsp4j.DidCloseTextDocumentParams
 import org.eclipse.lsp4j.DidOpenTextDocumentParams
 import org.eclipse.lsp4j.DidSaveTextDocumentParams
+import org.eclipse.lsp4j.jsonrpc.messages.Either
 import org.eclipse.lsp4j.services.TextDocumentService
+import java.util.concurrent.CompletableFuture
 
 class MockTextDocumentService : TextDocumentService {
 
@@ -23,4 +28,16 @@ class MockTextDocumentService : TextDocumentService {
     override fun didSave(params: DidSaveTextDocumentParams) {
         println("didSave called for: ${params.textDocument.uri}")
     }
+
+    override fun completion(position: CompletionParams): CompletableFuture<Either<List<CompletionItem>, CompletionList>> {
+        println("completion called for: ${position.textDocument.uri} at position: ${position.position}")
+
+        val completionList = CompletionList()
+        val item1 = CompletionItem("fakeItem1").apply { detail = "This is a fake completion item for testing" }
+        val item2 = CompletionItem("fakeItem2").apply { detail = "Another fake completion item" }
+        completionList.items = listOf(item1, item2)
+
+        return CompletableFuture.completedFuture(Either.forRight(completionList))
+    }
+
 }
